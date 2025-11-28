6fbe21a2af1142c5cb1ad4c0fd1a7d3af19fdaae
Nestor Gomez <nestorcode4004@gmail.com>
2025-11-28 16:18:19 -0500

    update clipboard

--- src/lib/copyClipBoard.jsx
+++ src/lib/copyClipBoard.jsx
@@ -1,7 +1,15 @@
 import { toast } from 'sonner'
-export const copyClipBoard = value => {
-	navigator.clipboard.writeText(value)
-	toast.success('Contenido copiado al portapapeles', {
-		description: 'Presiona Ctrl + V para pegar',
-	})
+import { copy } from 'clipboard-copy'
+export const copyClipBoard = async value => {
+	try {
+		await copy('Texto a copiar')
+		toast.success('Contenido copiado al portapapeles', {
+			description: 'Presiona Ctrl + V para pegar',
+		})
+	} catch (err) {
+		console.error('Error:', err)
+		toast.error('Error al copiar el contenido', {
+			description: 'No se pudo copiar el contenido',
+		})
+	}
 }
